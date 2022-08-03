49355974a1aedf54cce0ecf06aee45329f402860
vl0011 <vl0011@hallym.ac.kr>
2022-08-03 19:23:47 +0900

    s

---
+++ Broker/Broker.cs
@@ -0,0 +1,16 @@
+using System;
+
+using MQTTnet.Diagnostics;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+
+
+namespace Broker
+{
+    public class Broker
+    {
+        private bool useLog;
+        
+    }
+}
---
+++ BrokerTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace BrokerTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
